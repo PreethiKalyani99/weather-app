e13f4165e6c088a3103fa0ab15996e8ba9bbd214
preethi <fabreztina@gmail.com>
2021-09-29 19:14:05 +0530

    Adding modified Weather.js file

--- Weather.js
+++ Weather.js
@@ -1,80 +1,92 @@
 import React from 'react';
-import './weather.css';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
+import './Weather.css';
+import axios from 'axios';
 
-function Weather(){
-  const [city, setCity] = useState("");
-  const [unit, setUnit] = useState("");
-  const [result, setResult] = useState("");
-
-  function handleSearch(e){
-    e.preventDefault();
-
-    let date = new Date();
-    let dayOfWeekNumber = date.getDay();
-    let nameOfDay;
-
-    switch(dayOfWeekNumber){
-      case 0: 
-      nameOfDay = 'Sunday';
-      break;
-  case 1:
-      nameOfDay = 'Monday';
-      break;
-  case 2:
-      nameOfDay = 'Tuesday';
-      break;
-  case 3:
-      nameOfDay = 'Wednesday';
-      break;
-  case 4:
-      nameOfDay = 'Thursday';
-      break;
-  case 5:
-      nameOfDay = 'Friday';
-      break;
-  case 6:
-      nameOfDay = 'Saturday';
-      break;
-  default:
-      nameOfDay = 'Sunday';
-      break;
-
-  }
-  fetch("http://api.openweathermap.org/data/2.5/weather?q="+city+"&units="+unit+"&appid=3564a10d88457911c12463b5dd4eb867")
-  .then(a => a.json())
-  .then(res => {
-    let image = `http://openweathermap.org/img/w/${res.weather[0].icon}.png`;
-
-    let temperature = <div>
-        <img id = "image" src = {image} /> 
-        <div id="result">
-          <h1>{Math.floor(res.main.temp)}{'\u00B0'}</h1>
-          {nameOfDay}<br/>
-          {res.weather[0].description}<br/>
-          High: {Math.ceil(res.main.temp_max)}{'\u00B0'}{'\t'}
-          Low: {Math.floor(res.main.temp_min)}{'\u00B0'}
-        </div>
-    </div>;
-    setResult(temperature);
-    console.log(setResult(temperature))
-    })
-  }
-  return(
-    <div id="output">
-      <form onSubmit = {handleSearch} id="forms">
-        <label>City Name</label>
-        <input type="text" placeholder="" value={city} onChange={(e)=>{setCity(e.target.value)}} required/>
-        <select id="units" name = 'Unit' value={unit} onChange={(e)=>{setUnit(e.target.value)}}>
-          <option value="imperial">Fahrenheit</option>
-          <option value="metric">Celcius</option>
-        </select>
-        <button type = "submit">Get Weather!</button>
-      </form>
-      <div>
-      {result}
-      </div>
-    </div>
-  )
+function DayOfWeekNumber(){
+    let num = new Date().getDay();
+    let weekNumberDay =  {
+        0 : "Sunday",
+        1 : "Monday",
+        2 : "Tuesday",
+        3 : "Wednesday",
+        4 : "Thursday",
+        5 : "Friday",
+        6 : "Saturday"
+    }
+    return weekNumberDay[num];
 }
-export default Weather;
+
+function CityNameandTemperatureSelector(){
+    const [city, setCity] = useState("");
+    const [unit, setUnit] = useState("");
+    
+    return (
+        <div id="output">
+            <form id="forms">
+                <label>City Name</label>
+                <input required type="text" placeholder="" value={city} onChange={(e)=>{
+                    setCity(e.target.value);
+                    }}/>
+                <select id="units" name="Unit" value={unit} onChange={(e)=>{
+                    setUnit(e.target.value);
+                     }}>
+                    <option value="metric">Celcius</option>
+                    <option value="imperial">Fahrenheit</option>
+                </select>
+            </form>
+            <GetWeatherInfo city={city} unit={unit}/>
+        </div>
+    )
+}
+
+function GetWeatherInfo(city,unit){
+   const [datas, setData] = useState([]);
+
+   useEffect(() =>{
+       fetchLink();
+   })
+
+   const fetchLink = async () =>{
+        try{
+            const result = await axios.get("http://api.openweathermap.org/data/2.5/weather?q="+city.city+"&units="+unit.unit+"&appid=3564a10d88457911c12463b5dd4eb867");
+            setData(result.data);
+        }
+        catch(e){
+            console.log(e);
+        }
+   }
+   return <DisplayWeatherInfo res={datas}/>
+}
+    
+function DisplayWeatherInfo(res){
+    const [result, setResult] = useState("");
+
+    function handleSearch(e){
+        e.preventDefault();
+        let image = `http://openweathermap.org/img/w/${res.res.weather[0].icon}.png`;
+        
+        let weatherInfo = <div>
+            <img id="image" src={image} alt="weather icon"/>
+            <div id="result">
+            <h1>{Math.floor(res.res.main.temp)}{'\u00B0'}</h1>
+              <DayOfWeekNumber/><br/>
+              {res.res.weather[0].description}<br/>
+              High: {Math.ceil(res.res.main.temp_max)}{'\u00B0'}{'\t'}
+              Low: {Math.floor(res.res.main.temp_min)}{'\u00B0'}
+            </div>
+        </div>
+        setResult(weatherInfo);
+    }
+    return (
+        <div>
+            <form id="form">
+                <button type="button" onClick={handleSearch}>Get Weather!</button>
+            </form>
+            <div>
+                {result}
+            </div>
+        </div>
+    )
+}
+export default CityNameandTemperatureSelector;
